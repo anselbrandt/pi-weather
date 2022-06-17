a1127782d714cefbfda03dad9ace1c7815ecdc16
ansel brandt <ansel@anselbrandt.com>
2022-06-17 02:19:04 -0400

    Update weather.js

--- weather.js
+++ weather.js
@@ -2,11 +2,23 @@
 
 const fetch = require("node-fetch");
 const sense = require("sense-hat-led");
-const num = require("./digitsMap");
 
 const X = [64, 64, 64]; // white
 const O = [0, 0, 0]; // black
 const B = [0, 0, 64]; // blue
+
+const num = [
+[X,X,X,O,O,O,O,O,X,O,X,O,O,O,O,O,X,O,X,O,O,O,O,O,X,O,X,O,O,O,O,O,X,X,X,],
+[O,O,X,O,O,O,O,O,O,O,X,O,O,O,O,O,O,O,X,O,O,O,O,O,O,O,X,O,O,O,O,O,O,O,X,],
+[X,X,X,O,O,O,O,O,O,O,X,O,O,O,O,O,X,X,X,O,O,O,O,O,X,O,O,O,O,O,O,O,X,X,X,],
+[X,X,X,O,O,O,O,O,O,O,X,O,O,O,O,O,X,X,X,O,O,O,O,O,O,O,X,O,O,O,O,O,X,X,X,],
+[X,O,X,O,O,O,O,O,X,O,X,O,O,O,O,O,X,X,X,O,O,O,O,O,O,O,X,O,O,O,O,O,O,O,X,],
+[X,X,X,O,O,O,O,O,X,O,O,O,O,O,O,O,X,X,X,O,O,O,O,O,O,O,X,O,O,O,O,O,X,X,X,],
+[X,X,X,O,O,O,O,O,X,O,O,O,O,O,O,O,X,X,X,O,O,O,O,O,X,O,X,O,O,O,O,O,X,X,X,],
+[X,X,X,O,O,O,O,O,O,O,X,O,O,O,O,O,O,O,X,O,O,O,O,O,O,O,X,O,O,O,O,O,O,O,X,],
+[X,X,X,O,O,O,O,O,X,O,X,O,O,O,O,O,X,X,X,O,O,O,O,O,X,O,X,O,O,O,O,O,X,X,X,],
+[X,X,X,O,O,O,O,O,X,O,X,O,O,O,O,O,X,X,X,O,O,O,O,O,O,O,X,O,O,O,O,O,X,X,X,],
+];
 
 const city = "qc-147";
 const lang = "e";
