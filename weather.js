fcff3680b57bd97a1b8d9fe620ac99011f7caf2a
ansel brandt <ansel@anselbrandt.com>
2022-06-27 06:52:42 -0400

    Update weather.js

--- weather.js
+++ weather.js
@@ -7,6 +7,7 @@ const X = [64, 64, 64]; // white
 const O = [0, 0, 0]; // black
 const B = [0, 0, 64]; // blue
 
+// prettier-ignore
 const num = [
 [X,X,X,O,O,O,O,O,X,O,X,O,O,O,O,O,X,O,X,O,O,O,O,O,X,O,X,O,O,O,O,O,X,X,X,],
 [O,O,X,O,O,O,O,O,O,O,X,O,O,O,O,O,O,O,X,O,O,O,O,O,O,O,X,O,O,O,O,O,O,O,X,],
